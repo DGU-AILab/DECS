4e4f6f03c8e59fa5aecd44247b735726ba8ae6aa
parkmingyun99 <griffinland9@naver.com>
2023-09-26 02:23:09 +0000

    cuda11.8 and tensor2.13.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,21 @@
 # sudo docker logs [container_name] 으로 로그 확인 가능
 echo "hello entrypoint!"
 
+echo -e "\n\n -------------------------------------------------------------------------------------------------------------------------------------------- 
+\n\n 안녕하세요. 동국대학교 AI lab 입니다. \n\n 해당 컨테이너는 sudo권한을 가집니다. 
+\n 따라서, 컨테이너 또는 컨테이너가 실행중인 로컬서버에 해가 되는 행동을 할 시 사용자분에게 전적인 책임이 있음을 안내드립니다. 
+\n 디렉토리 또는 파일을 삭제하는 행위 또는 root 권한을 통해 시스템을 수정하는 명령어 등을 조심히 사용해주시면 됩니다.  
+\n 추가로, 사용자분의 디렉토리는 별도로 백업되지 않습니다. 중요한 파일의 경우 주기적으로 백업해 주시기 바랍니다. 감사합니다.
+\n (이미지 버전 : decs:1.4) \n\n
+--------------------------------------------------------------------------------------------------------------------------------------------\n" > /etc/motd
 # 유저 계정 생성
 if ! id "$USER_ID" >/dev/null 2>&1; then
     echo "No User account detected..."
 
     # 유저 계정을 생성, 홈폴더는 decs폴더로 설정
     useradd -s /bin/bash -d /$USER_ID $USER_ID
-    # sudo 권한을 주되, 자신 소유가 아닌 폴더를 삭제하는 shell command를 제한
-    echo "$USER_ID ALL=(ALL) NOPASSWD:ALL, !/bin/rm" >> /etc/sudoers
+    # sudo 권한을 준다.
+    echo "$USER_ID ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
     # 홈폴더 생성
     # skeleton 파일을 복사하여, 유저명이 tf-docker 로 표시되는 것을 방지
@@ -23,10 +30,11 @@ if ! id "$USER_ID" >/dev/null 2>&1; then
     echo "user account config done..."
     
     # sshd 설정도 바꾼다.
-    # 서버관리자와 유저계정의 ssh 접속을 허용
+    # 서버관리자와 유저계정의 ssh 접속을 허용 및 다중접속 허용
     sed -i "/^#PermitRootLogin/a AllowUsers svmanager" /etc/ssh/sshd_config
     sed -i "/^#PermitRootLogin/a AllowUsers $USER_ID" /etc/ssh/sshd_config
-    service ssh restart
+    sed -i 's/^UsePAM yes/UsePAM no/' /etc/ssh/sshd_config
+    
     echo "ssh change done..."
 
 fi
@@ -58,7 +66,8 @@ usermod -u $UID -g $UID $USER_ID
 # readme 안내문 생성
 echo "Hello Decs, 동국대학교 GPU 서버 컨테이너 서비스 decs 입니다." > /home/$USER_ID/readme_decs.txt
 
-# jupyterlab 설정파일 수정
+# ssh restart
+service ssh restart
 
 # jupyter lab 에서 생성한 ipynb 파일을 저장할 디렉토리 생성 (없는경우만 신규 생성)
 if [ ! -d "/home/$USER_ID/decs_jupyter_lab" ]; then
@@ -74,30 +83,29 @@ echo "trying jupyter lab..."
 nohup /opt/anaconda3/bin/jupyter lab --NotebookApp.token=decs --config=/jupyter_config/jupyter_notebook_config.py >/dev/null 2>&1 &
 echo "jupyter lab listening!"
 
-# xrdp의 터미널이 안켜지는 오류 자동 해결 : 터미널로 xface 를 선택
-# update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal
+# auditd 설치
+sudo apt update
+sudo apt install -y auditd
 
-# xrdp를 자동으로 시작
-service xrdp-sesman start
-service xrdp start
+# /etc/audit/audit.rules 파일에 줄 추가
+# sed -i "/^#-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid=$USER_ID -k rm_commands" /etc/audit/audit.rules
+echo "-a always,exit -F arch=b64 -S unlink -S unlinkat -S rename -S renameat -F auid=$USER_ID -k rm_commands" >> /etc/audit/audit.rules
+
+# history 명령어 칠 때 명령어를 입력한 시간이 같이 나오게끔 하는 명령어
+echo 'HISTTIMEFORMAT="[%Y-%m-%d %H:%M:%S] "' >> /etc/profile
+echo 'export HISTTIMEFORMAT' >> /etc/profile
+
+# history -w 현재시간.txt파일을 만들고, /var/log/audit로 이동하는 부분임. 사용자가 로그아웃 할 때
+echo 'cd ~' >> /home/$USER_ID/.bash_logout
+echo 'current_time=$(date +%Y-%m-%d_%H-%M-%S)' >> /home/$USER_ID/.bash_logout
+echo 'history -w $current_time.txt' >> /home/$USER_ID/.bash_logout
+echo 'sudo mv $current_time.txt /var/log/audit/' >> /home/$USER_ID/.bash_logout
 
 # 유저 개인폴더 안에 프로그램을 모두 설치하고 나면, 유저 개인폴더의 모든 파일의 소유자를 유저로 변경. 시간이 약간 소요됨(재귀로 모든 파일의 권한을 변경.)
 chown -R "$UID:$UID" "/home/$USER_ID"
 chmod -R 700 "/home/$USER_ID"
 echo "decs chown change done..."
 
-# 재시작 오류를 자동으로 해결
-pid_file="/var/run/xrdp/xrdp.pid"
-if [ -f "$pid_file" ]; then
-    rm -f "$pid_file"
-fi
-sesman_file="/var/run/xrdp/xrdp-sesman.pid"
-if [ -f "$sesman_file" ]; then
-    rm -f "$sesman_file"
-fi
-
-
-    
 
 #entrypoint.sh 를 실행하고 나서 컨테이너가 Exit 하지 않게함
 tail -F /dev/null
