040130c485fbc72185ca0b49d6a6b33ff380d782
pkhyrn268 <bokddang@dgu.ac.kr>
2025-03-04 14:30:13 +0900

    Add message for checking slack notice

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,12 +58,17 @@ fi
 
 # 로그인시 안내문 생성
 cat <<EXPL >> /etc/bash.bashrc
+
 echo -e "\e[0;33m"
 cat <<EOF
 서버에서 사용 중인 데이터는 언제 장애가 발생해서 사라질지 모릅니다. 항상 중요한 데이터는 서버 밖에 백업해두시기 바랍니다.
 Data on the server can be lost at any time due to unexpected failures. Always back up important data outside the server.
+
+Slack에 공지 후 24시간 이내에 확인 및 응답이 없어 발생하는 모든 불이익은 사용자 본인 책임입니다.
+You are solely responsible for any disadvantages resulting from failing to check and respond within 24 hours of a Slack notice.
 EOF
 echo -e "\e[0m"
+
 EXPL
 
 # ssh restart
