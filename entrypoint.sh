dbd58ab8ea5b769377061e67f6ffd795bfc6136f
sssuhyeon <ckc020506@gmail.com>
2026-04-29 00:15:18 +0900

    fix: 인덴트 간격 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,19 +37,19 @@ if ! id "$USER_ID" >/dev/null 2>&1; then
 
 else
 
-# 기존 유저 디렉토리의 /.bashrc에서 conda initialize 블록과 경고 주석을 제거
-sed -i \
-    -e "/# !!! Do NOT delete the conda initialize comments below\. !!!/d" \
-    -e "/# !!! Do NOT add anything inside the conda initialize block\. It will be removed on container restart\/update\. !!!/d" \
-    -e "/# >>> conda initialize >>>/,/# <<< conda initialize <<</d" \
-    /home/$USER_ID/.bashrc
+    # 기존 유저 디렉토리의 /.bashrc에서 conda initialize 블록과 경고 주석을 제거
+    sed -i \
+        -e "/# !!! Do NOT delete the conda initialize comments below\. !!!/d" \
+        -e "/# !!! Do NOT add anything inside the conda initialize block\. It will be removed on container restart\/update\. !!!/d" \
+        -e "/# >>> conda initialize >>>/,/# <<< conda initialize <<</d" \
+        /home/$USER_ID/.bashrc
 
-# /root/.bashrc에서 conda initialize 블록 추출해서 /home/$USER_ID/.bashrc에 추가
-{
-    echo "# !!! Do NOT delete the conda initialize comments below. !!!"
-    echo "# !!! Do NOT add anything inside the conda initialize block. It will be removed on container restart/update. !!!"
-    sed -n '/# >>> conda initialize >>>/,/# <<< conda initialize <<</p' /root/.bashrc
-} >> /home/$USER_ID/.bashrc
+    # /root/.bashrc에서 conda initialize 블록 추출해서 /home/$USER_ID/.bashrc에 추가
+    {
+        echo "# !!! Do NOT delete the conda initialize comments below. !!!"
+        echo "# !!! Do NOT add anything inside the conda initialize block. It will be removed on container restart/update. !!!"
+        sed -n '/# >>> conda initialize >>>/,/# <<< conda initialize <<</p' /root/.bashrc
+    } >> /home/$USER_ID/.bashrc
 
 fi
 
